b632fae66267e16a9ce3c7d65d15793ecbe1c512
iveltier <iveltier@mailbox.com>
2025-09-03 20:46:10 +0200

    Backup on: Wed Sep  3 08:46:10 PM CEST 2025

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,11 +1,11 @@
 -- Aktiviert LSP für Lua
-vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "lua_ls", "asm-lsp" })
 -- Autocompletion aktivieren (Neovim 0.11+)
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
 		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-			vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'noinsert', 'fuzzy', 'popup' }
+			vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
 			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
 		end
 		-- Keybindings für LSP-Funktionen
@@ -15,6 +15,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set('i', '<C-Space>', function()
+			vim.lsp.completion.get()
+		end)
 	end,
 })
 -- Formatierung beim Speichern
