bd4f0bc070a2b30aa71b06dbfffa7777ab734f35
iveltier <iveltier@mailbox.com>
2025-12-08 16:46:57 +0100

    Backup on: Mon Dec  8 04:46:57 PM CET 2025

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,28 +2,3 @@ require("config.keybinds")
 require("config.lazy")
 require("config.options")
 vim.cmd("colorscheme tokyonight")
-
-local null_ls = require("null-ls")
-
-null_ls.setup({
-	sources = {
-		-- Prettier über Mason
-		null_ls.builtins.formatting.prettierd.with({
-			extra_args = { "--semi", "true" }, -- erzwingt Semikolons
-		}),
-
-		-- oder ESLint Autofix
-		null_ls.builtins.diagnostics.eslint_d,
-		null_ls.builtins.code_actions.eslint_d,
-	},
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
-})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,36 +1,67 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		dependencies = {
-			"mason-org/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			servers = {
-				lua_ls = {
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" }
-							},
-						},
-					},
-				},
-				ts_ls = {},
-				eslint = {},
-				tailwindcss = {},
-			},
-		},
-		config = function(_, opts)
-			require("mason").setup()
-			require("mason-lspconfig").setup(
-				{ ensure_installed = { "lua_ls" } }
-			)
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "jose-elias-alvarez/null-ls.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      -- Mason Setup
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",      -- für JS/TS
+          "eslint",        -- optional
+          "tailwindcss",   -- falls du Tailwind nutzt
+        },
+      })
 
-			for server, config in pairs(opts.servers) do
-				vim.lsp.config(server, config)
-				vim.lsp.enable(server)
-			end
-		end
-	},
+      local lspconfig = require("lspconfig")
+
+      -- LSP Server Setup
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+          },
+        },
+      })
+
+      lspconfig.tsserver.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.tailwindcss.setup({})
+
+      -- null-ls Setup für Formatierung
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          -- Prettier Daemon (installiert via :MasonInstall prettierd)
+          null_ls.builtins.formatting.prettierd.with({
+            extra_args = { "--semi", "true" }, -- erzwingt Semikolons
+          }),
+
+          -- Optional: ESLint für zusätzliche Checks
+          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.code_actions.eslint_d,
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
 }
+
