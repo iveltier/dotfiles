fae7435e54f25acb1b5d7c8813b5d041ff496e67
iveltier <iveltier@mailbox.org>
2026-02-07 23:56:52 +0100

    Backup on: Sat Feb  7 11:56:52 PM CET 2026

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -4,8 +4,19 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- debug
+map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "DEBUGGER toggle debugger" })
+map("n", "<leader>dr", "<cmd> DapContinue <CR>", { desc = "DEBUGGER start or continue debugger" })
+
+-- oil
 map("n", "<leader>cd", vim.cmd.Oil, { desc = "OIL enable oil tree" })
+
+-- idk
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+vim.keymap.set({ "n", "t" }, "<A-c>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end)
