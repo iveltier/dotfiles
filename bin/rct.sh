cf9defea86eacece3a598ce04ad1449e4febb94f
iveltier <iveltier@mailbox.org>
2026-02-12 15:40:49 +0100

    Backup on: Thu Feb 12 03:40:49 PM CET 2026

--- bin/rct.sh
+++ bin/rct.sh
@@ -1,4 +1,16 @@
+#!/usr/bin/env bash
 
-#!/bin/bash
-hyprctl keyword monitor eDP-1,800x600,0x0,1
-killall waybar
+# 1. Bildschirm verkleinern
+hyprctl keyword monitor "eDP-1,800x600,0x0,1"
+
+# 2. Spiel starten (komplett detached, kein Terminal)
+GAME_PATH="$HOME/.wine/drive_c/ProgramData/Microsoft/Windows/Start Menu/Programs/Atari/RollerCoaster Tycoon/RollerCoaster Tycoon.lnk"
+nohup wine "$GAME_PATH" >/dev/null 2>&1 &
+
+# 3. Warten, bis das Spiel nicht mehr in Hyprland-Clients auftaucht
+while hyprctl clients | grep -qi "rct\|roller\|wine"; do
+    sleep 1
+done
+
+# 4. Bildschirm zurücksetzen
+hyprctl reload
