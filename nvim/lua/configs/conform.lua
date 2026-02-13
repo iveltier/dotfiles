2f92f95462b608a9c5e50e21827ff23f26877519
iveltier <iveltier@mailbox.org>
2026-02-13 13:18:45 +0100

    Backup on: Fri Feb 13 01:18:45 PM CET 2026

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -2,7 +2,6 @@ local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     css = { "prettier" },
-    cpp = { "prettier" },
     html = { "prettier" },
   },
 
--- nvim/lua/plugins/minty.lua
+++ nvim/lua/plugins/minty.lua
@@ -1,9 +1,10 @@
 -- color picker
 return {
-  { "nvzone/volt", lazy = true },
+  { "nvzone/volt", lazy = true, disabled = true },
 
   {
     "nvzone/minty",
+    disabled = true,
     cmd = { "Shades", "Huefy" },
   },
 }
