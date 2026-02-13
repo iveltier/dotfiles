5e9c96371899237bd34c5efbfd777d554ae7ad12
iveltier <iveltier@mailbox.org>
2026-02-13 02:37:11 +0100

    Backup on: Fri Feb 13 02:37:11 AM CET 2026

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,13 +1,14 @@
-require("nvchad.configs.lspconfig").defaults()
-
+-- require("nvchad.configs.lspconfig").defaults()
+--
 -- local servers = { "html", "cssls", "clangd", "eslint_d" }
 --
 -- vim.lsp.enable(servers)
 
 --- Nutze mason-lspconfig's automatisches Setup
-require("mason-lspconfig").setup_handlers {
-  -- Default handler für alle installierten Server
-  function(server_name)
-    vim.lsp.enable(server_name)
-  end,
-} --read :h vim.lsp.config for changing options of lsp servers
+--read :h vim.lsp.config for changing options of lsp servers
+require("nvchad.configs.lspconfig").defaults()
+
+-- Automatisch alle Mason-LSPs aktivieren
+for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+  vim.lsp.enable(server)
+end
