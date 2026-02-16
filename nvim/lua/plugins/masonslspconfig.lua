4a25ad9602bf89a6310d62e88fd03a826304e5bd
iveltier <iveltier@mailbox.org>
2026-02-16 20:50:34 +0100

    Backup on: Mon Feb 16 08:50:34 PM CET 2026

--- nvim/lua/plugins/masonslspconfig.lua
+++ nvim/lua/plugins/masonslspconfig.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {},
-  },
-
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    opts = {
-      automatic_installation = true,
+    {
+        "williamboman/mason.nvim",
+        opts = {},
     },
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "clangd", "html", "cssls", "vtsls" },
-        automatic_installation = false,
-      }
-    end,
-  },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim" },
+        opts = {
+            automatic_installation = true,
+        },
+        -- config = function()
+        --     require("mason-lspconfig").setup {
+        --         ensure_installed = { "clangd", "html", "cssls", "vtsls" },
+        --         automatic_installation = true,
+        --     }
+        -- end,
+    },
 }
