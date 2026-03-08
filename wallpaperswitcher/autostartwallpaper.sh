6cd4012231fff26b27a0d64a779e03057f19b7d8
iveltier <iveltier@mailbox.org>
2026-03-08 14:08:56 +0100

    Backup on: Sun Mar  8 02:08:56 PM CET 2026

---
+++ wallpaperswitcher/autostartwallpaper.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+LAST="$HOME/.cache/last_wallpaper"
+
+if [[ -f "$LAST" ]]; then
+    file=$(cat "$LAST")
+    ext="${file##*.}"
+    ext="${ext,,}"
+
+    pkill mpvpaper 2>/dev/null
+
+    case "$ext" in
+        jpg|jpeg|png)
+            swww img "$file" --transition-type any --transition-duration 1.0
+            ;;
+        gif|mp4)
+            mpvpaper -o "loop=yes" "*" "$file"
+            ;;
+    esac
+fi
+
