ab54cace934336132ada7d16947701437599a3b0
iveltier <iveltier@mailbox.org>
2026-03-10 01:06:15 +0100

    Backup on: Tue Mar 10 01:06:15 AM CET 2026

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,1 +1,11 @@
 require "nvchad.autocmds"
+--
+-- Öffne Oil automatisch, wenn nvim mit einem Verzeichnis gestartet wird
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local arg = vim.fn.argv(0)
+    if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+      vim.cmd("Oil " .. arg)
+    end
+  end,
+})
